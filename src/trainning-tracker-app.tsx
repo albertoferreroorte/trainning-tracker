a25adedeb6041d58638a551f0e285069c8fb64e3
Alberto Ferrero Orte <AFERRERO@intern.osp-dd.de>
2023-09-19 12:36:33 +0200

    added app title and timeline button in sticky nav header

--- src/trainning-tracker-app.tsx
+++ src/trainning-tracker-app.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Grid, Tab, Tabs } from '@mui/material';
+import { Button, createTheme, Grid, Tab, Tabs, ThemeProvider, Typography } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
 import { AppRouter } from './router';
+import { TimelineOutlined } from '@mui/icons-material';
 
 const tabs = [
   {
@@ -23,36 +24,65 @@ export const TrainningTrackerApp: React.FC = () => {
     setValue(newValue);
   };
 
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Roboto', 'Oswald'
+      ].join(','),
+    },});
+
   useEffect(() => {
     const tab = tabs.find(t => t.path === location.pathname) || tabs[1];
     setValue(tab.value);
   }, [location.pathname, value]);
 
   return (
-    <Grid
-      alignItems="center"
-      container
-      direction="column"
-      height="100%"
-      justifyContent="center"
-      spacing={0}
-      width="100%"
-    >
-      <Grid color='dark' item sx={{ mt: 3 }}>
-        <Tabs
-          value={value}
-          onChange={ handleChange}
-          aria-label="nav tabs"
-          textColor='inherit'
-          TabIndicatorProps={{ style: { background: "orange" } }}
+    <ThemeProvider theme={ theme }>
+      <Grid
+        alignItems="center"
+        container
+        direction="column"
+        height="100%"
+        justifyContent="center"
+        spacing={0}
+        width="100%"
+      >
+        <Grid
+          alignItems='center'
+          color='dark'
+          container
+          justifyContent='space-between'
+          position='sticky'
+          top={ 0 }
+          sx={{ backgroundColor: 'white' }}
+          zIndex={ 2 }
         >
-          <Tab label="Courses" component={ Link } to="/courses" />
-          <Tab label="Students" component={ Link } to="/students" />
-        </Tabs>
+          <Typography fontSize='large' fontFamily='Oswald' sx={{ mx: 3 }}>Trainning tracker</Typography>
+          <Tabs
+            value={value}
+            onChange={ handleChange}
+            aria-label="nav tabs"
+            textColor='inherit'
+            TabIndicatorProps={{ style: { background: "orange" } }}
+            sx={{ mt: 3 }}
+          >
+            <Tab label="Courses" component={ Link } to="/courses" />
+            <Tab label="Students" component={ Link } to="/students" />
+          </Tabs>
+          <Button
+            aria-label="overview"
+            color="primary"
+            variant="outlined"
+            startIcon={<TimelineOutlined fontSize='large' />}
+            sx={{ mx: 3 }}
+          >
+            Timeline
+          </Button>
+        </Grid>
+        <Grid item sx={{ width: '100%' }}>
+          <AppRouter />
+        </Grid>
       </Grid>
-      <Grid item sx={{ width: '100%' }}>
-        <AppRouter />
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
